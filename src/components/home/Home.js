d881d6263cd5966491f873df738f4426e59047fa
Nikola Lukic <iamnikolalukic@gmail.com>
2017-06-30 14:33:17 +0200

    Http request example, removed sanctuary, added folktale data.task && data.either

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,14 +3,33 @@ import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { updateTestData, updateTest2Data } from './HomeContainer';
+import { testApi, testApiInit } from '../../../config/api.js';
+import { updateTest1Data, updateTest2Data } from './HomeContainer';
+import Task from 'data.task';
+// import Either from 'data.either';
 
-// Check destructuring ( no reassignment lint)
 const Home = (props) => {
+
+const httpRequest = (url, init) =>
+  new Task((rej, res) => // Task monad for handling async requests
+    fetch(url, init)
+      .then(r => r.json())
+      .then(d => res(d))
+      .catch(e => rej(e)));
+
+const getData = (url, init) =>
+  httpRequest(url, init)
+    .map(r => r.title)
+    .fork(e => console.log(e), // fork error or response from Task monad
+          x => props.updateTest2Data(x));
+
   return (
     <View>
-      <TouchableOpacity onPress={() => props.updateTest2Data('Test 2')}>
-        <Text style={styles.text}>{props.testData}</Text>
+      <TouchableOpacity onPress={() => props.updateTest1Data('Updated test 1 data')}>
+        <Text style={styles.text}>{props.test1Data}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => getData(testApi, testApiInit)}>
+        <Text style={styles.text}>{props.test2Data}</Text>
       </TouchableOpacity>
     </View>
   );
@@ -22,11 +41,12 @@ const Home = (props) => {
 // };
 
 const stateToProps = state => ({
-  testData: state.homeReducer.testData
+  test1Data: state.homeReducer.test1Data,
+  test2Data: state.homeReducer.test2Data
 });
 
 const dispatchToProps = dispatch => ({
-  updateTestData: bindActionCreators(updateTestData, dispatch),
+  updateTest1Data: bindActionCreators(updateTest1Data, dispatch),
   updateTest2Data: bindActionCreators(updateTest2Data, dispatch)
 });
 
@@ -34,6 +54,6 @@ export default connect(stateToProps, dispatchToProps)(Home);
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 50
+    fontSize: 30
   }
 });
--- src/components/home/HomeContainer.js
+++ src/components/home/HomeContainer.js
@@ -1,20 +1,23 @@
 // Actions
-const TEST = 'home/TEST';
+const TEST_1 = 'home/TEST_1';
 const TEST_2 = 'home/TEST_2';
 
 // Initial State
 const initialState = {
-  testData: 'Some data for testing...'
+  test1Data: 'Test 1 data',
+  test2Data: 'Test 2 data',
 };
 
 // Reducer
 const homeReducer = (state = initialState, action) =>
-  action.type === TEST ? ({ ...state, testData: action.val }) :
-  action.type === TEST_2 ? ({ ...state, testData: action.val }) :
+  action.type === TEST_1 ? ({ ...state, test1Data: action.str }) :
+  action.type === TEST_2 ? ({ ...state, test2Data: action.str }) :
   state;
 
 // Action Creators
-export const updateTestData = val => ({ type: TEST, val });
-export const updateTest2Data = val => ({ type: TEST_2, val });
+export const updateTest1Data = str => ({ type: TEST_1, str });
+export const updateTest2Data = str => ({ type: TEST_2, str });
+
+
 
 export default homeReducer;

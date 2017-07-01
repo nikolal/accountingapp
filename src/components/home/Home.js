ceee0ad0938b33502003ed9dc8ade4c1223bd31f
Nikola Lukic <iamnikolalukic@gmail.com>
2017-07-01 19:03:49 +0200

    Using Fluture monad for async

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,33 +5,23 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { testApi, testApiInit } from '../../../config/api.js';
 import { updateTest1Data, updateTest2Data } from './HomeContainer';
-import Task from 'data.task';
-// import Either from 'data.either';
+import Future from 'fluture';
 
 const Home = (props) => {
 
-// Maybe we should move httpRequest function to separate folder since it will be reused a lot:
-// example: utility || utilityHelpers.js || helperFunctions.js
-// Todo: Handle null check with data.Maybe or data.Either
-const httpRequest = (url, init) =>
-  new Task((rej, res) => // Task monad for handling async requests
-    fetch(url, init)
-      .then(r => r.json())
-      .then(d => res(d))
-      .catch(e => rej(e)));
-
-const getData = (url, init) =>
-  httpRequest(url, init)
-    .map(r => r.title)
-    .fork(e => console.log(e), // fork error or response from Task monad
-          x => props.updateTest2Data(x));
+  // Todo: Handle null check with data.Maybe or data.Either
+  const httpRequest = (url, init) =>
+    Future.tryP(() => fetch(url, init)) // Future monad wraps fetch
+      .chain(res => Future.tryP(_ => res.json()))
+      .map(x => x.title)
+      .fork(console.error, x => props.updateTest2Data(x)); // Unwraps from Future monad
 
   return (
     <View>
       <TouchableOpacity onPress={() => props.updateTest1Data('Updated test 1 data')}>
         <Text style={styles.text}>{props.test1Data}</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => getData(testApi, testApiInit)}>
+      <TouchableOpacity onPress={() => httpRequest(testApi, testApiInit)}>
         <Text style={styles.text}>{props.test2Data}</Text>
       </TouchableOpacity>
     </View>

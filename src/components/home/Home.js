6090c93204771a8d901689647c034a8d26b8ad33
Radovan Hajdukovic <hajdukovic.radovan@gmail.com>
2017-07-02 05:34:04 +0200

    add real example to get things easier

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,41 +1,56 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, TextInput, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { updateTest1Data, updateTest2Data } from './HomeContainer';
+import PropTypes from 'prop-types';
+import { increaseValue, updateUsername, githubName } from './HomeContainer';
 import Future from 'fluture';
 import S from 'sanctuary';
 
 const Home = (props) => {
 
   // Async request example
-  const httpRequest = () =>
-    Future.encaseP(fetch)('https://api.github.com/users/dajk') // Future monad wraps fetch
+  const httpRequest = (username) =>
+    Future.encaseP(fetch)(`https://api.github.com/users/${username}`) // Future monad wraps fetch
       .chain(res => Future.tryP(_ => res.json()))
       .map(x => x.name)
       .fork(console.error, // Fork extracts value from Future monad (error or value)
-        x => props.updateTest2Data(S.fromMaybe('/', S.toMaybe(x)))); // If x is null/undefined, Maybe will return '/',
+        x => props.githubName(S.fromMaybe('/', S.toMaybe(x)))); // If x is null/undefined, Maybe will return '/',
 
   return (
-    <View>
-      <TouchableOpacity onPress={() => props.updateTest1Data('Updated test 1 data')}>
-        <Text style={styles.text}>{props.test1Data}</Text>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => props.increaseValue()}>
+        <Text style={styles.text}>{props.value}</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => httpRequest()}>
-        <Text style={styles.text}>{props.test2Data}</Text>
+      <TouchableOpacity onPress={() => httpRequest(props.username)}>
+        <TextInput
+          style={{width: 200, height: 40, borderColor: 'green', borderWidth: 1 }}
+          onChangeText={text => props.updateUsername(text)}
+          value={props.username}
+        />
+        <Text>GET NAME</Text>
       </TouchableOpacity>
+      <Text style={styles.text}>{props.name}</Text>
     </View>
   );
 };
 
+Home.propTypes = { // eslint-disable-line
+  value: PropTypes.number,
+  username: PropTypes.string,
+  name: PropTypes.string
+};
+
 const stateToProps = state => ({
-  test1Data: state.homeReducer.test1Data,
-  test2Data: state.homeReducer.test2Data
+  value: state.homeReducer.value,
+  username: state.homeReducer.username,
+  name: state.homeReducer.name
 });
 
 const dispatchToProps = dispatch => ({
-  updateTest1Data: bindActionCreators(updateTest1Data, dispatch),
-  updateTest2Data: bindActionCreators(updateTest2Data, dispatch)
+  increaseValue: bindActionCreators(increaseValue, dispatch),
+  updateUsername: bindActionCreators(updateUsername, dispatch),
+  githubName: bindActionCreators(githubName, dispatch)
 });
 
 export default connect(stateToProps, dispatchToProps)(Home);
@@ -43,5 +58,10 @@ export default connect(stateToProps, dispatchToProps)(Home);
 const styles = StyleSheet.create({
   text: {
     fontSize: 30
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
--- src/components/home/HomeContainer.js
+++ src/components/home/HomeContainer.js
@@ -1,23 +1,31 @@
 // Actions
-const TEST_1 = 'home/TEST_1';
-const TEST_2 = 'home/TEST_2';
+const INCREASE_VALUE = 'home/INCREASE_VALUE';
+const UPDATE_USERNAME = 'home/UPDATE_USERNAME';
+const GET_GITHUB_NAME = 'home/GET_GITHUB_NAME';
 
 // Initial State
 const initialState = {
-  test1Data: 'Test 1 data',
-  test2Data: 'Test 2 data',
+  value: 0,
+  username: 'dajk'
 };
 
 // Reducer
-const homeReducer = (state = initialState, action) =>
-  action.type === TEST_1 ? ({ ...state, test1Data: action.str }) :
-  action.type === TEST_2 ? ({ ...state, test2Data: action.str }) :
-  state;
+const homeReducer = (state = initialState, action) => { // eslint-disable-line
+  switch (action.type) {
+    case INCREASE_VALUE:
+      return { ...state, value: state.value + 1 };
+    case UPDATE_USERNAME:
+      return { ...state, username: action.username };
+    case GET_GITHUB_NAME:
+      return { ...state, name: action.name };
+    default:
+      return state;
+  }
+};
 
 // Action Creators
-export const updateTest1Data = str => ({ type: TEST_1, str });
-export const updateTest2Data = str => ({ type: TEST_2, str });
-
-
+export const increaseValue = () => ({ type: INCREASE_VALUE });
+export const updateUsername = username => ({ type: UPDATE_USERNAME, username });
+export const githubName = name => ({ type: GET_GITHUB_NAME, name });
 
 export default homeReducer;

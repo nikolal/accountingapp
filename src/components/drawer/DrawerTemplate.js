e08aedfe967a58ffb11f0ad5e669df582ab314cb
Nikola Lukic <iamnikolalukic@gmail.com>
2018-02-15 11:15:07 +0100

    Add default styles

--- src/components/drawer/DrawerTemplate.js
+++ src/components/drawer/DrawerTemplate.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { SimpleLineIcons }  from '@expo/vector-icons';
+import { metrics, colors, fonts } from '../../theme';
 
 class DrawerTemplate extends Component {
 
   state = {
-    drawerList: [{ name: 'Home' }, { name: 'About' }]
+    drawerList: [
+      { key: 'Tabs', icon: 'home', name: 'Home' },
+      { key: 'Login', icon: 'login', name: 'Login' }
+    ]
   };
 
+  goToScreen = key => this.props.navigation.navigate(key);
+
   renderList = (item, index) =>
-    <View key={index}>
-      <Text>{item.name}</Text>
-    </View>
+    <TouchableOpacity key={index} onPress={() => this.goToScreen(item.key)} style={styles.item}>
+      <SimpleLineIcons name={item.icon} size={20} color="black" />
+      <Text style={styles.text}>{item.name}</Text>
+    </TouchableOpacity>
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         {this.state.drawerList.map(this.renderList)}
       </View>
     );
@@ -22,3 +30,18 @@ class DrawerTemplate extends Component {
 }
 
 export default DrawerTemplate;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: metrics.smallMargin
+  },
+  text: {
+    fontSize: fonts.size.medium,
+    marginLeft: metrics.smallMargin
+  }
+});

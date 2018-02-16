d22c5272e966cc067aafddea3cee3519cc864cf6
nikolal <iamnikolalukic@hotmail.com>
2018-02-16 23:23:39 +0100

    styling screen news detail

--- src/components/news-detail/NewsDetail.js
+++ src/components/news-detail/NewsDetail.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, Dimensions } from 'react-native';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import { metrics, colors, fonts } from '../../theme';
+
 
 class NewsDetail extends Component {
 
@@ -11,26 +13,32 @@ class NewsDetail extends Component {
 
   renderArticle = (item, index) =>
     item.type === 'text' ?
-      <Text key={index}>{item.value}</Text> :
+      <Text key={index} style={styles.articleParagraphs}>{item.value}</Text> :
     item.type === 'image' ?
-      <Image key={index} style={{width: 50, height: 50}} source={{uri: item.value}} /> :
+      <Image key={index} style={styles.image} source={{uri: item.value}} /> :
     null;
 
   render(){
     const { article } = this.props;
     return (
-      <View>
-        <Image
-          style={{width: 50, height: 50}}
-          source={{uri: article.image}}
-        />
-        <View>
-          <Text>{article.title}</Text>
-          <Text>{article.description}</Text>
-          <Text>{article.date}</Text>
+      <ScrollView style={styles.container}>
+        <View style={styles.articleContainer}>
+          <View style={styles.coverImageContainer}>
+            <Image
+              resizeMode="cover"
+              style={styles.coverImage}
+              source={{uri: article.image}}
+            />
+            <View style={styles.dateTimeContainer}>
+              <Text style={styles.titleText}>{article.title}</Text>
+              <Text style={styles.dateText}>{article.date}</Text>
+            </View>
+          </View>
+          <Text style={styles.descriptionText}>{article.description}</Text>
+          {article.paragraphs.map(this.renderArticle)}
         </View>
-        {article.paragraphs.map(this.renderArticle)}
-      </View>
+
+      </ScrollView>
     );
   }
 }
@@ -44,3 +52,63 @@ const stateToProps = state => ({
 });
 
 export default connect(stateToProps, null)(NewsDetail);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.lightGrey,
+    padding: metrics.small,
+  },
+  articleContainer: {
+    // alignItems: 'center',
+    marginTop: metrics.medium,
+    padding: metrics.medium,
+    backgroundColor: colors.white,
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+  },
+  coverImageContainer: {
+    flex: 1,
+      // height: Dimensions.get('window').height / 4,
+  },
+  coverImage: {
+    height: Dimensions.get('window').height / 3.5,
+  },
+  dateTimeContainer: {
+    position: 'absolute',
+    bottom: metrics.small,
+    left: metrics.small
+  },
+  titleText: {
+    color: colors.white,
+    fontSize: fonts.size.huge,
+    fontWeight: fonts.weight.large,
+    marginBottom: metrics.small,
+  },
+  dateText: {
+    color: colors.white,
+    fontSize: fonts.size.small,
+    marginBottom: metrics.small,
+  },
+  descriptionText: {
+    fontSize: fonts.size.small,
+    fontWeight: fonts.weight.large,
+    marginVertical: metrics.small
+  },
+  articleParagraphs: {
+    marginBottom: metrics.small
+  },
+  image: {
+    alignSelf: 'center',
+    width: 150,
+    height: 150,
+    marginBottom: metrics.small
+  }
+});
--- src/components/news/NewsContainer.js
+++ src/components/news/NewsContainer.js
@@ -4,10 +4,10 @@ const SAVE_ARTICLE = 'news/SAVE_ARTICLE';
 // Initial State
 const initialState = {
   news: [{
-    title: 'Title',
+    title: 'Title titel ltik tuy;le maiu aid lorem ',
     description: 'Description lorem ipsum...',
     date: '30.04.2018',
-    image: 'https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/68dd54ca-60cf-4ef7-898b-26d7cbe48ec7/10-dithering-opt.jpg',
+    image: 'http://chimeiusa.com/wp-content/uploads/2017/06/How-to-Find-the-Best-Inventory-Management-Accounting-Software-for-Your-Small-Business-5-Questions-to-Ask-800x534.jpg',
     paragraphs: [{
       type: 'text',
       value: 'lorem imsum Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ab sequi rem quaerat maxime. Repellendus tempore commodi esse a dolore, quibusdam delectus eveniet deserunt necessitatibus,'
@@ -19,10 +19,10 @@ const initialState = {
       value: 'lorem Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ab sequi rem quaerat maxime. Repellendus tempore commodi esse a dolore, quibusdam delectus eveniet deserunt necessitatibus, imsum'
     }]
   },{
-    title: 'Title2',
+    title: 'Title2 is the ftiksm lorem ipsum',
     description: 'Description lorem ipsum2...',
     date: '30.05.2018',
-    image: 'https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/68dd54ca-60cf-4ef7-898b-26d7cbe48ec7/10-dithering-opt.jpg',
+    image: 'http://www.dmu.ac.uk/webimages/business-and-law-images/course-images/bal/accounting-and-finance.jpg',
     paragraphs: [{
       type: 'text',
       value: 'lorem ipsum2 lorem <iosum> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, ab sequi rem quaerat maxime. Repellendus tempore commodi esse a dolore, quibusdam delectus eveniet deserunt necessitatibus, omnis molestias illo iure iste.</iosum>'

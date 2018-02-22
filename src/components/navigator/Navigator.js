6e0cf81b108709f594a6d7112420c58ecc83785e
nikolal <iamnikolalukic@hotmail.com>
2018-02-22 22:59:58 +0100

    Created new component TaxesDetail in folder taxes-detail, Some problems with path

--- src/components/navigator/Navigator.js
+++ src/components/navigator/Navigator.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import { metrics, colors, fonts } from '../../theme/index.js';
 import NewsDetail from '../news-detail/NewsDetail.js';
-import TaxesDetail from '../taxes-detail/TaxesDetail.js';
+import TaxesDetail from '../taxes-detail/TaxesDetail';
 import Settings from '../settings/Settings.js';
 import Tabs from '../tabs/Tabs.js';
 import About from '../about/About.js';

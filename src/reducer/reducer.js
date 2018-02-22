b95b4d64151f80433507b21f5eb13743641dc554
nikolal <iamnikolalukic@hotmail.com>
2018-02-22 22:30:20 +0100

    Creating component Taxes.js, reducer taxesReducer/TaxesContainer.js

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import homeReducer from '../components/home/HomeContainer.js';
 import newsReducer from '../components/news/NewsContainer.js';
+import taxesReducer from '../components/taxes/TaxesContainer';
 
 export default combineReducers({
   homeReducer,
-  newsReducer
+  newsReducer,
+  taxesReducer
 });

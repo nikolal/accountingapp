0a017f4e64430097c98cbd64ce927152f265b280
Milanalu <lukic.milana13@gmail.com>
2018-04-26 18:00:59 +0200

    Added font Open Sans - regular, bold, Light

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { ActivityIndicator } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './src/store/store.js';
 import Drawer from './src/components/drawer/Drawer.js';
+import { Font } from 'expo';
 
-const App = () => {
-  return (
-    <Provider store={store}>
-      <Drawer />
-    </Provider>
-  );
-};
+class App extends Component {
+
+  state = {
+    fontLoaded: false
+  }
+
+  componentDidMount = () => {
+    Font.loadAsync({
+      'openSansRegular': require('./assets/fonts/OpenSans-Regular.ttf'),
+      'openSansBold': require('./assets/fonts/OpenSans-Bold.ttf'),
+      'openSansLight':require('./assets/fonts/OpenSans-Light.ttf'),
+    })
+    .then(() => this.setState({ fontLoaded: true}));
+  }
+
+  render () {
+    if (this.state.fontLoaded) {
+      return (
+        <Provider store={store}>
+          <Drawer />
+        </Provider>
+      );
+    } else {
+      return <ActivityIndicator size="small" color="grey" animating />;
+    }
+  }
+}
 
 export default App;

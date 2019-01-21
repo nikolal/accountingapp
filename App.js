4224d3d391d5df58ea6338c3e43c9750f3ac6318
Milanalu <lukicmilana13@gmail.com>
2019-01-21 19:12:33 +0100

    Cesh Images & Fonts

--- App.js
+++ App.js
@@ -1,34 +1,26 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, Asset, Image } from 'react-native';
+import { Image, View, Dimensions } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './src/store/store.js';
 import Drawer from './src/components/drawer/Drawer.js';
-import { Font } from 'expo';
-
-const cacheImages = (images) =>
-  images.map(image => {
-    typeof image === 'string' ?
-      Image.prefetch(image) :
-      Asset.fromModule(image).downloadAsync();
-  });
+import { Font, SplashScreen, Asset, AppLoading } from 'expo';
 
 class App extends Component {
 
   state = {
-    fontLoaded: false
+    isSplashReady: false,
+    isAppReady: false
   }
 
-  componentDidMount = () => {
-    Font.loadAsync({
-      'openSansRegular': require('./assets/fonts/OpenSans-Regular.ttf'),
-      'openSansBold': require('./assets/fonts/OpenSans-Bold.ttf'),
-      'openSansLight':require('./assets/fonts/OpenSans-Light.ttf'),
-    })
-    .then(() => this.setState({ fontLoaded: true}));
+  cacheSplasheResources = async () => {
+    const img = require('./assets/images/caseCalculations.png');
+    return Asset.fromModule(img).downloadAsync();
   }
 
-  async _loadAssetsAsync() {
-    const imageAssets = cacheImages([
+  cacheResourcesAsync = async () => {
+    SplashScreen.hide();
+
+    const images = [
       require('./assets/images/drawerBackground.png'),
       require('./assets/images/backgroundImage.png'),
       require('./assets/images/calculationsPanel.png'),
@@ -45,21 +37,52 @@ class App extends Component {
       require('./assets/images/milkaSlika.png'),
       require('./assets/images/servicesImagex.png'),
       require('./assets/images/taxesImage.png'),
-    ]);
+    ];
 
-    await Promise.all([...imageAssets]);
+    const cacheImages = images.map(image => {
+        return Asset.fromModule(image).downloadAsync();
+    });
+
+    const cacheFonts = Font.loadAsync({
+      'openSansRegular': require('./assets/fonts/OpenSans-Regular.ttf'),
+      'openSansBold': require('./assets/fonts/OpenSans-Bold.ttf'),
+      'openSansLight':require('./assets/fonts/OpenSans-Light.ttf'),
+    });
+
+    await Promise.all([cacheImages, cacheFonts]);
+    this.setState({ isAppReady: true });
   }
 
   render () {
-    if (this.state.fontLoaded) {
+    if (!this.state.isSplashReady) {
       return (
-        <Provider store={store}>
-          <Drawer />
-        </Provider>
+        <AppLoading
+          startAsync={this.cacheSplasheResources}
+          onFinish={() => this.setState({ isSplashReady: true })}
+          onError={console.warn}
+          autoHideSplash={false}
+        />
       );
-    } else {
-      return <ActivityIndicator size="small" color="grey" animating />;
     }
+
+    if (!this.state.isAppReady){
+      return (
+        <Image
+          style={{
+            width: Dimensions.get('window').width,
+            height: Dimensions.get('window').height
+          }}
+          source={require('./assets/images/drawerBackground.png')}
+          onLoad={this.cacheResourcesAsync}
+        />
+      );
+    }
+
+    return (
+      <Provider store={store}>
+        <Drawer />
+      </Provider>
+    );
   }
 }
 

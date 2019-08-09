8d00b187f01059ce421ba1bd707551eb76b96338
Milanalu <lukicmilana13@gmail.com>
2019-08-09 09:22:42 +0200

    Right margin smaller

--- src/components/something/Something.js
+++ src/components/something/Something.js
@@ -127,7 +127,7 @@ const styles = StyleSheet.create({
     marginBottom: metrics.medium
   },
   shareImage: {
-    marginRight: metrics.medium,
+    marginRight: metrics.small,
     color: '#bdbdbd',
   },
 });
